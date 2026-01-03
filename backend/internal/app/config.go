130ec6ddde587aaf98105352a5ed268fc891631a
morrowsol <morrowsol@MacBookPro.lan>
2026-01-03 21:29:33 +0200

    feat: update config parsing function

--- backend/internal/app/config.go
+++ backend/internal/app/config.go
@@ -2,48 +2,61 @@ package app
 
 import (
 	"fmt"
-	"os"
+	"strings"
 
-	"gopkg.in/yaml.v3"
+	"github.com/spf13/viper"
 )
 
 func ReadConfig(path string) (Config, error) {
-	file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+	config, err := readConfig(path)
 	if err != nil {
-		return Config{}, fmt.Errorf("open config: %w", err)
+		return Config{}, err
 	}
 
-	defer file.Close()
-
-	var config Config
-
-	if err = yaml.NewDecoder(file).Decode(&config); err != nil {
-		return Config{}, fmt.Errorf("read config: %w", err)
-	}
-
-	if err = config.Validate(); err != nil {
+	if err = config.validate(); err != nil {
 		return Config{}, fmt.Errorf("validate config: %w", err)
 	}
 
 	return config, nil
 }
 
-type Config struct {
-	Server      ServerConfig   `yaml:"server"`
-	DebugServer ServerConfig   `yaml:"debug_server"`
-	Postgres    PostgresConfig `yaml:"postgres"`
+func readConfig(path string) (Config, error) {
+	v := viper.New()
+
+	v.SetConfigFile(path)
+
+	v.AutomaticEnv()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := v.ReadInConfig(); err != nil {
+		return Config{}, fmt.Errorf("read config: %w", err)
+	}
+
+	var config Config
+
+	if err := v.Unmarshal(&config); err != nil {
+		return Config{}, fmt.Errorf("unmarshal config: %w", err)
+	}
+
+	return config, nil
 }
 
-func (c Config) Validate() error {
-	if err := c.Server.Validate(); err != nil {
+type Config struct {
+	Server      ServerConfig   `mapstructure:"server"`
+	DebugServer ServerConfig   `mapstructure:"debugServer"`
+	Postgres    PostgresConfig `mapstructure:"postgres"`
+}
+
+func (c Config) validate() error {
+	if err := c.Server.validate(); err != nil {
 		return fmt.Errorf("validate server config: %w", err)
 	}
 
-	if err := c.DebugServer.Validate(); err != nil {
+	if err := c.DebugServer.validate(); err != nil {
 		return fmt.Errorf("validate debug server config: %w", err)
 	}
 
-	if err := c.Postgres.Validate(); err != nil {
+	if err := c.Postgres.validate(); err != nil {
 		return fmt.Errorf("validate postgres config: %w", err)
 	}
 
@@ -51,25 +64,55 @@ func (c Config) Validate() error {
 }
 
 type ServerConfig struct {
-	Address string `yaml:"address"`
+	Port string `mapstructure:"port"`
 }
 
-func (c ServerConfig) Validate() error {
-	if c.Address == "" {
-		return fmt.Errorf("address is required")
+func (c ServerConfig) validate() error {
+	if c.Port == "" {
+		return fmt.Errorf("port is required")
 	}
 
 	return nil
 }
 
 type PostgresConfig struct {
-	ConnectionString string `yaml:"connection_string"`
+	Database string `mapstructure:"db"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
 }
 
-func (c PostgresConfig) Validate() error {
-	if c.ConnectionString == "" {
-		return fmt.Errorf("connection string is required")
+func (c PostgresConfig) validate() error {
+	if c.Database == "" {
+		return fmt.Errorf("database name is required")
+	}
+
+	if c.User == "" {
+		return fmt.Errorf("database user is required")
+	}
+
+	if c.Password == "" {
+		return fmt.Errorf("database password is required")
+	}
+
+	if c.Host == "" {
+		return fmt.Errorf("database host is required")
+	}
+
+	if c.Port == "" {
+		return fmt.Errorf("database port is required")
 	}
 
 	return nil
+}
+
+func (c PostgresConfig) GetConnectionString() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		c.User,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.Database,
+	)
 }
