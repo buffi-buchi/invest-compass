92f58c508fb57e75473893d8961ef32dacb095b1
Mikhail Baev <supamiha@gmail.com>
2026-02-02 23:11:54 +0700

    Store models

--- backend/internal/provider/postgres/portfolio_store.go
+++ backend/internal/provider/postgres/portfolio_store.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
@@ -15,16 +16,39 @@ import (
 var (
 	//go:embed queries/get_portfolios_by_user_id.sql
 	getPortfoliosByUserIDQuery string
+	//go:embed queries/create_portfolio.sql
+	createPortfolioQuery string
 )
 
 type PortfolioStore struct {
-	db *pgxpool.Pool
+	db  *pgxpool.Pool
+	id  func() (uuid.UUID, error)
+	now func() time.Time
 }
 
 func NewPortfolioStore(db *pgxpool.Pool) *PortfolioStore {
 	return &PortfolioStore{
-		db: db,
+		db:  db,
+		id:  func() (uuid.UUID, error) { return uuid.NewV7() },
+		now: func() time.Time { return time.Now().UTC() },
 	}
+}
+
+func (s *PortfolioStore) Create(ctx context.Context, ps model.Portfolio) (model.Portfolio, error) {
+	id, err := s.id()
+	if err != nil {
+		return model.Portfolio{}, fmt.Errorf("create portfolio_security ID: %w", err)
+	}
+
+	ps.ID = id
+	ps.CreateTime = s.now()
+
+	_, err = s.db.Exec(ctx, createPortfolioQuery, ps.ID, ps.UserID, ps.Name, ps.CreateTime)
+	if err != nil {
+		return model.Portfolio{}, fmt.Errorf("upsert portfolio_security: %w", err)
+	}
+
+	return ps, nil
 }
 
 func (s *PortfolioStore) GeByUserID(
