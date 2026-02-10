c3e5edb40f04c82d3f742114e32831a6773ab183
Mikhail Baev <supamiha@gmail.com>
2026-02-11 00:01:17 +0700

    Sql query for test data

---
+++ backend/internal/provider/postgres/testdata/create_test_index.sql
@@ -0,0 +1,9 @@
+INSERT INTO indexes (id, index_code, name, create_time)
+VALUES ('6f1b2a6e-9c3e-4a2d-8b7f-3e5f1c9a4d21',
+        'MOEXBC',
+        'MOEXBC',
+        '2025-09-10T00:00:00Z'::TIMESTAMPTZ),
+       ('a2c7e9d4-1b5f-4e8a-9d3c-7f6a2b0e1c45',
+        'IMOEX',
+        'IMOEX',
+        '2025-09-10T00:00:00Z'::TIMESTAMPTZ);
