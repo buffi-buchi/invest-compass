74d478e0c3b3a14fc2e18b4128e3ac540f81a4ac
Mikhail Baev <supamiha@gmail.com>
2026-02-20 13:38:48 +0700

    fix review comment

--- backend/migrations/20260218051225_create_indexes_table.sql
+++ backend/migrations/20260218051225_create_indexes_table.sql
@@ -3,12 +3,11 @@
 
 CREATE TABLE IF NOT EXISTS indexes
 (
-    id          UUID        NOT NULL,
-    ticker      VARCHAR     NOT NULL UNIQUE,
+    ticker      VARCHAR,
     name        VARCHAR     NOT NULL,
     create_time TIMESTAMPTZ NOT NULL DEFAULT now(),
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (ticker)
 );
 
 CREATE INDEX IF NOT EXISTS indexes_code_idx ON indexes (ticker);
