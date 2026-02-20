693bf37c7c85e987598e8d67b6afc7dade1aaebe
Mikhail Baev <supamiha@gmail.com>
2026-02-20 14:20:38 +0700

    add security store migration

--- backend/internal/provider/postgres/queries/create_index.sql
+++ backend/internal/provider/postgres/queries/create_index.sql
@@ -1,2 +1,2 @@
 INSERT INTO indexes (ticker, name, create_time)
-VALUES ($1, $2, $3, $4)
+VALUES ($1, $2, $3)
---
+++ backend/internal/provider/postgres/queries/create_security.sql
@@ -0,0 +1,2 @@
+INSERT INTO securities (ticker, short_name, create_time)
+VALUES ($1, $2, $3)
